a9ef9470b46865d77bee848de8631b08b6f596a1
BonosoLaraUTN <bonoso.lara@alumnos.frm.utn.edu.ar>
2024-07-23 18:08:24 -0300

    primer commit

---
+++ src/main/java/com/ticketera/backticketeradervinsa/BackTicketeraDervinsaApplication.java
@@ -0,0 +1,13 @@
+package com.ticketera.backticketeradervinsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackTicketeraDervinsaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackTicketeraDervinsaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ticketera/backticketeradervinsa/BackTicketeraDervinsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticketera.backticketeradervinsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackTicketeraDervinsaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
